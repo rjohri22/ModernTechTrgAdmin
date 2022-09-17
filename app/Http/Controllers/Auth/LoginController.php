83a9fb0c2687e18a4b74631458d9b71cdb9359a1
Ismail FSD <ismailfsd2@gmail.com>
2022-09-17 11:31:44 +0500

    Update Code

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      * @var string
      */
 
-    protected $redirectTo = '\loginverification';
+    protected $redirectTo = 'loginverification';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/loginverification';
+    protected $redirectTo = 'loginverification';
 
     /**
      * Create a new controller instance.
