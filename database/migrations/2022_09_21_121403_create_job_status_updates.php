6e9dcbf25f7ded7afec285556ee1c8b53f2643e3
salman_zain <salmanzain786@outlook.com>
2022-09-21 18:04:41 +0500

    Job Status Update Section

--- database/migrations/2022_09_21_121403_create_job_status_updates.php
+++ database/migrations/2022_09_21_121403_create_job_status_updates.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobStatusUpdateTbl extends Migration
+class CreateJobStatusUpdate extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateJobStatusUpdateTbl extends Migration
      */
     public function up()
     {
-        Schema::create('job_status_update_tbl', function (Blueprint $table) {
+        Schema::create('job_status_updates', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->integer('emp_id',false, true)->nullable();
@@ -28,6 +28,6 @@ class CreateJobStatusUpdateTbl extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('job_status_update_tbl');
+        Schema::dropIfExists('job_status_updates');
     }
 }
